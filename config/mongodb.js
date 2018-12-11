0be93e4433871fa99dec83c45c22619101b632a6
unknown <gurvan.t@orange.fr>
2018-12-11 12:01:52 +0100

    config mongodb refait au propre en attente du chemin exact vers le server

---
+++ config/mongodb.js
@@ -0,0 +1,4 @@
+//Connexion base de donnée Mongo 
+module.exports = {
+    url: 'mongodb://10.44.100.39:27017//MoviesMongoDB'
+}
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dbConfig = require('./config/mongodb.js');
+const mongoose = require('mongoose');
 
-const app = expres();
+const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}))
 
@@ -13,4 +15,16 @@ app.get('/',(req, res) => {
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
+});
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
 });
